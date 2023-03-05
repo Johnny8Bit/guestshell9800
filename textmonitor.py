2722ee143b4953f56909fef007941e22afdbd43c
Johnny8Bit <33725046+Johnny8Bit@users.noreply.github.com>
2023-03-05 11:50:00 +0100

    renamed file

---
+++ textmonitor.py
@@ -0,0 +1,37 @@
+from datetime import datetime
+
+import json
+import sys
+import os
+
+from flask import Flask
+from flask import request
+from tabulate import tabulate
+
+import logging
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.ERROR)
+
+data_stream = Flask(__name__)
+
+
+def generate_output(monitor_data):
+
+    if platform == "win32": os.system("cls")
+    elif platform == "linux": os.system("clear")
+    print("Last update:", str(datetime.now())[:-7], "\n")
+    headings = ["AP Name", "Slot", "Radio", "Ch.Util %", "Clients", "Ch.Changes", "Last Ch.Change"]
+    print(tabulate(monitor_data, headers=headings))
+
+
+@data_stream.route('/monitor', methods = ['POST'])
+def visualize():
+
+    generate_output(json.loads(request.data))
+    return 'OK', 200
+
+
+if __name__ == '__main__':
+
+    platform = sys.platform
+    data_stream.run(host='0.0.0.0', port=9800, ssl_context='adhoc', debug=False)
