48cd05027aeb64f1fd111da103e900251eb1dc3f
Johnny8Bit <33725046+Johnny8Bit@users.noreply.github.com>
2023-03-05 11:47:53 +0100

    renamed file

--- datastream.py
+++ datastream.py
@@ -1,37 +1,66 @@
-from datetime import datetime
 
+import requests
+import urllib3
+import time
 import json
 import sys
-import os
+import re
+import cli
 
-from flask import Flask
-from flask import request
-from tabulate import tabulate
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-import logging
-log = logging.getLogger('werkzeug')
-log.setLevel(logging.ERROR)
+send_top = 20
 
-data_stream = Flask(__name__)
+monitor_ip = "192.168.1.6"
+monitor_port = "9800"
+monitor_api = f"https://{monitor_ip}:{monitor_port}/monitor"
 
-
-def generate_output(monitor_data):
-
-    if platform == "win32": os.system("cls")
-    elif platform == "linux": os.system("clear")
-    print("Last update:", str(datetime.now())[:-7], "\n")
-    headings = ["AP Name", "Slot", "Radio", "Ch.Util %", "Clients", "Ch.Changes", "Last Ch.Change"]
-    print(tabulate(monitor_data, headers=headings))
+include = "AP Name|Slot ID|Radio Type|Channel Utilization|Attached Clients|Channel Change Count|Last Channel Change Time"
 
 
-@data_stream.route('/monitor', methods = ['POST'])
-def visualize():
+def parse_output(cli_output):
 
-    generate_output(json.loads(request.data))
-    return 'OK', 200
+    monitor_data = []
+    for ap_data in cli_output:
+
+        ap_metrics = []
+        try:
+            ap_name = re.match("\s+: (\S+)", ap_data).group(1)
+        except AttributeError:
+            continue
+        ap_slot = re.search("Slot ID\s+: (\S+)", ap_data).group(1)
+        ap_radio = re.search("Radio Type\s+: (.+)", ap_data).group(1)
+        ap_chutil = re.search("Channel Utilization\s+: (\S+)", ap_data).group(1)
+        ap_chutil = int(ap_chutil[:-1])
+        ap_clients = re.search("Attached Clients\s+: (\S+)", ap_data).group(1)
+        try:
+            ap_changes = re.search("Channel Change Count\s+: (.+)", ap_data).group(1)
+        except AttributeError:
+            ap_changes = "-"
+        try:
+            ap_lastchange = re.search("Last Channel Change Time\s+: (.+)", ap_data).group(1)
+        except AttributeError:
+            ap_lastchange = "-"
+
+        ap_metrics=(ap_name, ap_slot, ap_radio, ap_chutil, ap_clients, ap_changes, ap_lastchange)
+        monitor_data.append(ap_metrics)
+
+    return monitor_data
 
 
-if __name__ == '__main__':
+while True:
 
-    platform = sys.platform
-    data_stream.run(host='0.0.0.0', port=9800, ssl_context='adhoc', debug=False)
+    try:
+        output = parse_output(cli.cli(f"show ap auto-rf dot11 5ghz | include {include}").split("AP Name"))
+        output_sorted = sorted(output, key=lambda x: x[3], reverse=True)[0:send_top] #sort by ch.util
+        try:
+            post = requests.post(monitor_api, data=json.dumps(output_sorted), verify=False, timeout=2)
+            print(monitor_ip, post.status_code)
+        except requests.exceptions.ConnectTimeout:
+            print(monitor_ip, "Timeout")
+        except requests.exceptions.ConnectionError:
+            print(monitor_ip, "Connection error")
+        time.sleep(5)
+    except KeyboardInterrupt:
+        sys.exit()
+
